d97b71239c425a7ba96361372f2e42ae87faa625
Nicholas Shi <nicholas.shi01@gmail.com>
2021-11-05 15:45:42 -0400

    Removed react-router-native and added react-navigation instead

--- client/App.js
+++ client/App.js
@@ -2,13 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NativeRouter, Route, Link } from 'react-router-native';
+
+import HomeView from './pages/Home/index';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  // return (
+  //   <View style={styles.container}>
+  //     <Text h3>Open up App.js to start working on your app!</Text>
+  //     <StatusBar style="auto" />
+  //   </View>
+  // );
+
+  return <NativeRouter>
+    
+  </NativeRouter>
 }
 
 const styles = StyleSheet.create({
