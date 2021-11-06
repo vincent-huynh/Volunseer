4bf26337d8f68357cce0236b9b57d82dcefe0f87
Nicholas Shi <nicholas.shi01@gmail.com>
2021-11-06 08:15:53 -0400

    Added base template for org pages

---
+++ client/pages/OrgPages/OrgProfile/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ScrollView, View } from 'react-native';
+
+import PageBody from '../../../components/PageBody';
+import Spacer from '../../../components/Spacer';
+import SelectionButton from '../../../components/SelectionButton';
+import TextInput from '../../../components/TextInput';
+
+export default OrgProfileView = ({ navigator }) => {
+    <View style={{ flex: 1 }}>
+        <StatusBar barStyle="dark-content" />
+        <View style={{ position: "absolute", marginTop: 50, marginLeft: 20, zIndex: 1 }} >
+            <Icon name="back" size={16} color="#000" onPress={() => navigation.pop()} />
+        </View>
+
+        <ScrollView style={{ marginTop: SharedStyles.topPageMargin }}>
+            <PageBody>
+
+            </PageBody>
+        </ScrollView>
+    </View>
+}
