b50aa3b7c32b78353e93539262bd9a45f4d445e4
Vincent Huynh <motherlord@lawn-143-215-117-28.lawn.gatech.edu>
2021-11-11 14:24:47 -0500

    user registration, login, get info

---
+++ routes/api/users.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+// @route           GET /api/users/me
+// @desc            Get current user info
+// @access          Private
+router.get("/me", auth, (req, res) => {
+  return res.status(200).json({
+    user: req.user,
+  });
+});
+
+// @route           GET /api/users/:user_id
+// @desc            Get user info by id
+// @access          Private
+router.get("/:user_id", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.user_id).select(
+      "-guilds -password -isAdmin"
+    );
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: `Could not find user with id: ${req.params.user_id}` });
+    }
+    return res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      msg: "Error with getting user from database",
+    });
+  }
+});
+
+// @route           POST /api/users
+// @desc            Register an account. Make sure you include
+//                  name, email, password, and isVolunteer (boolean)
+// @access          Public
+router.post(
+  "/",
+  [
+    check("name", "Name is required").notEmpty(),
+    check("email", "You need a valid email").isEmail(),
+    check("password", "Password must be more than 6 characters long.").isLength(
+      { min: 6 }
+    ),
+    check("isVolunteer", "Need to specify if user is a volunteer").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let { name, email, password, isVolunteer } = req.body;
+
+    try {
+      email = email.toLowerCase();
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "user exists already" }] });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+        isVolunteer,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        process.env.jwtSecret,
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Something went wrong on the server...");
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-require('dotenv').config()
-const express = require('express');
-const connectDB = require('./db/db');
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db/db");
 const app = express();
+
+process.once("SIGUSR2", function () {
+  process.kill(process.pid, "SIGUSR2");
+});
+
+process.on("SIGINT", function () {
+  // this is only called on ctrl+c, not restart
+  process.kill(process.pid, "SIGINT");
+});
 
 // Required for body validation
 app.use(express.json({ extended: false }));
 // Connect to MongoDB Database
 connectDB();
 
-app.get('/', (req, res) => res.send('Reached API!'));
+app.get("/", (req, res) => res.send("Reached API!"));
 
 // Routes
-app.use('/api/test', require('./routes/api/test'));
+app.use("/api/test", require("./routes/api/test"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
