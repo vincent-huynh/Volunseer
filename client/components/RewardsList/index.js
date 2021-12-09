71b9fa731684f3d194310bb6eef0af325e75e1ee
Colin Tam <colintam@Colins-MacBook-Pro-2.local>
2021-12-08 21:23:05 -0500

    Rewards page now functional

--- client/components/RewardsList/index.js
+++ client/components/RewardsList/index.js
@@ -1,59 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
+import axios from 'axios';
+import setAuthToken from "../../util/setAuthToken.js";
+
+import { SharedStyles } from '../../style';
 
 import RewardItem from '../RewardItem';
 
-// Remove later when data from reliable source
-const sampleRewardsInfo = [
-    {
-        value: 70,
-        description: "$5 Starbucks"
-    },
-    {
-        value: 500,
-        description: "10% Macy Discount"
-    },
-    {
-        value: 70,
-        description: "$5 Starbucks"
-    },
-    {
-        value: 50,
-        description: "Small Coffee"
-    },
-    {
-        value: 10,
-        description: "Donate $1 to Charity of Choice"
-    },
-    {
-        value: 30,
-        description: "$1 McDonalds"
-    },
-    {
-        value: 1000,
-        description: "$10 Gift Card"
-    },
-    {
-        value: 50,
-        description: "Lottery Entry"
-    },
-    {
-        value: 500,
-        description: "Ice Cream Voucher"
-    },
-];
-
-export default RewardsList = ({ rewardsInfo=[] }) => {
-    if (rewardsInfo.length == 0)    // TODO: REMOVE later when sample data not needed
-        rewardsInfo = sampleRewardsInfo
-
-    return <View style={styles.container}>
-        {rewardsInfo.map((e, i) => <RewardItem cost={e.value} description={e.description} key={i}/>)}
+export default RewardsList = () => {
+    const [rewardsInfo, setRewardsInfo] = React.useState([]);
+    
+    useEffect(() => {
+        if (global.token) { 
+            setAuthToken(global.token);
+        }
+        if (rewardsInfo.length == 0)
+            axios.get("http://159.223.142.127:3001/api/rewards/all").then((res) => {
+                setRewardsInfo(res.data);
+            });
+    });
+    
+        
+    return <View>
+        {rewardsInfo.map((e, i) => <RewardItem cost={e.points} description={e.description} key={i}/>)}
     </View>
 }
 
-const styles = StyleSheet.create({
-    container: {
-
-    }
-});
+// const styles = StyleSheet.create({
+//     container: {
+        
+//     }
+// });
