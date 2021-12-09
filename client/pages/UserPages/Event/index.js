3b130d4d6657742610f0cd51bea75f822eee8284
Vincent Huynh <vincent.huynh123@gmail.com>
2021-12-08 23:26:19 -0500

    working rsvp

--- client/pages/UserPages/Event/index.js
+++ client/pages/UserPages/Event/index.js
@@ -11,7 +11,7 @@ import {
   Text,
   Image,
   Linking,
-  Platform
+  Platform,
 } from "react-native";
 
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -36,26 +36,22 @@ import image2 from "../../../assets/event_images/2.jpg";
 import image3 from "../../../assets/event_images/3.jpg";
 import image4 from "../../../assets/event_images/4.jpg";
 
-
 export default EventView = ({ routes, navigation }) => {
-
   console.log(global.markerData);
   console.log(global.markerData.location.coordinates[0]);
 
-  const images = [
-    image0, image1, image2, image3, image4
-  ];
-  const scheme = Platform.select({ ios: 'maps:0,0?q=', android: 'geo:0,0?q=' });
+  const images = [image0, image1, image2, image3, image4];
+  const scheme = Platform.select({ ios: "maps:0,0?q=", android: "geo:0,0?q=" });
   const latLng = `${global.markerData.location.coordinates[1]},${global.markerData.location.coordinates[0]}`;
-  const label = global.markerData.name; 
+  const label = global.markerData.name;
   const url = Platform.select({
     ios: `${scheme}${label}@${latLng}`,
-    android: `${scheme}${latLng}(${label})`
+    android: `${scheme}${latLng}(${label})`,
   });
-  
+
   return (
     <SafeAreaView>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar barStyle='dark-content' />
       <View
         style={{
           position: "absolute",
@@ -65,9 +61,9 @@ export default EventView = ({ routes, navigation }) => {
         }}
       >
         <Icon
-          name="back"
+          name='back'
           size={16}
-          color="#000"
+          color='#000'
           onPress={() => navigation.pop()}
         />
       </View>
@@ -75,29 +71,50 @@ export default EventView = ({ routes, navigation }) => {
       <PageBody>
         <SubheaderText text={global.markerData.name} />
         <Spacer height={20} />
-        <Image style={styles.eventImage} source={images[Math.floor(Math.random() * 5)]} />
+        <Image
+          style={styles.eventImage}
+          source={images[Math.floor(Math.random() * 5)]}
+        />
         <Spacer height={20} />
         <SubText text={global.markerData.description} />
         <Spacer height={30} />
 
-        <BubbleButton style={{ backgroundColor: "transparent", color: "blue" }} buttonText="Directions" btnOnPress={() => Linking.openURL(url) } />
-        <BubbleButton style={{ backgroundColor: "#A9DE90"}} buttonText="RSVP" />
+        <BubbleButton
+          style={{ backgroundColor: "transparent", color: "blue" }}
+          buttonText='Directions'
+          btnOnPress={() => Linking.openURL(url)}
+        />
+        <BubbleButton
+          style={{ backgroundColor: "#A9DE90" }}
+          buttonText='RSVP'
+          btnOnPress={() => {
+            if (global.token) {
+              setAuthToken(global.token);
+            }
+            axios
+              .put(
+                `http://159.223.142.127:3001/api/events/rsvp/${global.markerData._id}`
+              )
+              .then((res) => console.log("good"))
+              .catch((err) => console.error(err));
+          }}
+        />
       </PageBody>
     </SafeAreaView>
   );
 };
 
-const windowsWidth = Dimensions.get('window').width;
-const windowsHeight = Dimensions.get('window').height;
+const windowsWidth = Dimensions.get("window").width;
+const windowsHeight = Dimensions.get("window").height;
 const styles = StyleSheet.create({
   bubbleContainer: {
     width: "auto",
     margin: "2%",
     padding: "3%",
   },
-  
+
   eventImage: {
     width: windowsWidth * 0.9,
     height: windowsWidth * 0.6,
-}
+  },
 });
