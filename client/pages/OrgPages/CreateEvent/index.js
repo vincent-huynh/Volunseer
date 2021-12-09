6c19f69f412882ea75b6e77dcd977f99106f086d
Nicholas Shi <nicholas.shi01@gmail.com>
2021-12-08 23:02:59 -0500

    Added map selection option with coordinates on create event page

--- client/pages/OrgPages/CreateEvent/index.js
+++ client/pages/OrgPages/CreateEvent/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StatusBar, View } from 'react-native';
+import { ScrollView, StatusBar, View, Modal, StyleSheet, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 
 import PageBody from '../../../components/PageBody';
@@ -7,6 +7,9 @@ import Spacer from '../../../components/Spacer';
 import SelectionButton from '../../../components/SelectionButton';
 import TextInput from '../../../components/TextInput';
 import SubheaderText from '../../../components/SubheaderText';
+import BubbleButton from "../../../components/BubbleButton";
+
+import MapView, { Marker } from 'react-native-maps';
 
 import { SharedStyles } from '../../../style';
 
@@ -15,10 +18,25 @@ export default CreateEventView = ({ navigation }) => {
     const [description, setDescription] = React.useState("");
     const [numPointsAward, setNumPointsAward] = React.useState("");
     const [numVolunteers, setNumVolunteers] = React.useState("");
+    const [latitude, setLatitude] = React.useState(33.7756178);
+    const [longitude, setLongitude] = React.useState(-84.3984737);
+
+    const [region, setRegion] = React.useState({
+        latitude: 33.7756178,
+        longitude: -84.3984737,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+
+    const [isMapOpen, setIsMapOpen] = React.useState(false);
 
     const onCreateEvent = () => {
         
         navigation.pop();
+    }
+
+    const showMap = () => {
+        setIsMapOpen(!isMapOpen);
     }
 
     return <View style={{ flex: 1 }}>
@@ -34,11 +52,56 @@ export default CreateEventView = ({ navigation }) => {
                 <TextInput placeholder="Description" style={{height: "30%", flexWrap: "wrap"}} onInputChange={setDescription} />
                 <TextInput placeholder="Number of Points to Award" onInputChange={setNumPointsAward} />
                 <TextInput placeholder="Number of Volunteers Needed" onInputChange={setNumVolunteers} />
+                <BubbleButton style={{ borderRadius: 10 }} btnOnPress={ showMap } buttonText="Set Location" />
                 <Spacer height={SharedStyles.elementSpacing} />
                 <SelectionButton buttonText="Create Event" btnOnPress={ onCreateEvent } />
                 <Spacer height={SharedStyles.elementSpacing} />
                 <SelectionButton buttonText="Cancel" btnOnPress={() => navigation.pop()} />
             </PageBody>
         </ScrollView>
+
+
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={isMapOpen}
+            onRequestClose={() => setIsMapOpen(!isMapOpen)}
+        >
+            <View style={styles.modalContainer}>
+                <MapView style={styles.map} initialRegion={region} onRegionChangeComplete={setRegion}>
+                    <Marker draggable
+                        coordinate={{longitude, latitude}}
+                        onDragEnd={e => {
+                            setLongitude(e.nativeEvent.coordinate.longitude);
+                            setLatitude(e.nativeEvent.coordinate.latitude);
+                            setRegion({
+                                longitude: longitude,
+                                latitude: latitude,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421,
+                            });
+                        }}
+                    />
+                    <SelectionButton buttonText="Finish Location Selection" btnOnPress={() => { setIsMapOpen(!isMapOpen) }} />
+                </MapView>
+                
+            </View>
+        </Modal>
     </View>
 }
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+const styles = StyleSheet.create({
+    map: {
+        width: windowWidth,
+        height: windowHeight,
+        justifyContent: "flex-end",
+        alignItems: "center"
+      },
+    modalContainer: {
+        alignItems: "center",
+        position: "absolute",
+        bottom: 0,
+    }
+});
